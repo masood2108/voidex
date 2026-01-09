8c3c5c47dd06564aebcf8ea3dcaa5a7de9c5731d
Masood Hussain <masoodhussainr8@gmail.com>
2026-01-09 15:34:06 +0530

    1

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC-10lmBOg_NGX8fNqFohi-8TFbtM4MdHk",
-  authDomain: "voidex-ea820.firebaseapp.com",
-  projectId: "voidex-ea820",
-  storageBucket: "voidex-ea820.firebasestorage.app",
-  messagingSenderId: "97557019254",
-  appId: "1:97557019254:web:7be6c4347dcb70bcebbd57"
-};
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
 export const db = getFirestore(app)
